f754e6d4e76dac4b186715a2b8f3ac26702f6ba9
Li <776239423@qq.com>
2022-12-12 20:04:02 +0800

    更新

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,66 +1,116 @@
 <template>
-    <div class="app-container">
-        <el-table
-        :data="list"
-        style="width: 100%"
-        row-key="id"
-        border
-        lazy
-        :load="getChildrens"
-        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-            <el-table-column label="名称" width="230" align="left">
-            <template slot-scope="scope">
-            <span>{{ scope.row.name }}</span>
-            </template>
-            </el-table-column>
-
-            <el-table-column label="编码" width="220">
-            <template slot-scope="{row}">
-                    {{ row.dictCode }}
-            </template>
-            </el-table-column>
-            <el-table-column label="值" width="230" align="left">
-            <template slot-scope="scope">
-            <span>{{ scope.row.value }}</span>
-            </template>
-            </el-table-column>
-            <el-table-column label="创建时间" align="center">
-            <template slot-scope="scope">
-            <span>{{ scope.row.createTime }}</span>
-            </template>
-            </el-table-column>
-        </el-table>
+  <div class="app-container">
+    <!-- 导出按钮 -->
+    <div class="el-toolbar">
+      <div class="el-toolbar-body" style="justify-content: flex-start">
+        <a
+          href="http://localhost:8202/admin/cmn/dict/exportData"
+          target="_blank"
+        >
+          <el-button type="text"><i class="fa fa-plus" /> 导出</el-button>
+        </a>
+        <el-button type="text" @click="importData"
+          ><i class="fa fa-plus" /> 导入</el-button
+        >
+      </div>
     </div>
+    <!-- 文件上传对话框 -->
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+      <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+          <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:8202/admin/cmn/dict/importData'"
+            class="upload-demo"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">
+              只能上传xls文件，且不超过500kb
+            </div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false"> 取消 </el-button>
+      </div>
+    </el-dialog>
+
+    <el-table
+      :data="list"
+      style="width: 100%"
+      row-key="id"
+      border
+      lazy
+      :load="getChildrens"
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+    >
+      <el-table-column label="名称" width="230" align="left">
+        <template slot-scope="scope">
+          <span>{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="编码" width="220">
+        <template slot-scope="{ row }">
+          {{ row.dictCode }}
+        </template>
+      </el-table-column>
+      <el-table-column label="值" width="230" align="left">
+        <template slot-scope="scope">
+          <span>{{ scope.row.value }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.createTime }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 
 
 <script>
-import dict from '@/api/dict'
+import dict from "@/api/dict";
 export default {
-    data() {
-        return {
-            list:[] //数据字典列表数组
-        }
+  data() {
+    return {
+      dialogImportVisible: false, // 控制上传对话框是否显示
+      list: [], //数据字典列表数组
+    };
+  },
+  created() {
+    this.getDictList(1);
+  },
+  methods: {
+    // 点击导入后弹出上传对话框
+    importData() {
+      this.dialogImportVisible = true;
     },
-    created() {
-        this.getDictList(1)
+    // 文件上传成功后执行
+    onUploadSuccess() {
+      this.$message({
+        type: "success",
+        message: "导入成功!",
+      });
+      // 刷新页面
+      this.getDictList(1);
     },
-    methods: {
-        //数据字典列表
-        getDictList(id) {
-            dict.dictList(id)
-                .then(response => {
-                    this.list = response.data
-                })
-        },
-        getChildrens(tree, treeNode, resolve) {
-            dict.dictList(tree.id).then(response => {
-                resolve(response.data)
-            })
-        }
-    }
-}
+    //数据字典列表
+    getDictList(id) {
+      dict.dictList(id).then((response) => {
+        this.list = response.data;
+      });
+    },
+    getChildrens(tree, treeNode, resolve) {
+      dict.dictList(tree.id).then((response) => {
+        resolve(response.data);
+      });
+    },
+  },
+};
 </script>
 
 
